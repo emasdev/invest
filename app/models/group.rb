0a286e39a0574ed333b62b613f661d07218d69fb
Emas <emas.dev@gmail.com>
2020-09-30 06:07:27 -0500

    Create join table groups_investments

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,7 @@ class Group < ApplicationRecord
   validates :icon, format: { with: /.+\.(jpe?g|png)\z/, message: 'only JPEG,JPG and PNG are valid formats.' }, allow_blank: true
   
   belongs_to :user
-  # has_and_belongs_to_many :investments
+  has_and_belongs_to_many :investments
 
   scope :sort_by_name, -> { order(name: :asc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_103601) do
+ActiveRecord::Schema.define(version: 2020_09_30_110317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2020_09_30_103601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "groups_investments", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "investment_id", null: false
   end
 
   create_table "investments", force: :cascade do |t|
